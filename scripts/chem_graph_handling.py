2ebfc0375691fd035ea282d0906672b31df62e69
olgapokrovskaja-rd <olga.pokrovskaja@avicomp.com>
2026-01-05 12:14:04 +0100

    chem graph

--- scripts/chem_graph_handling.py
+++ scripts/chem_graph_handling.py
@@ -7,32 +7,32 @@ rsmi = '[CH3:1][CH:2]=[O:3].[CH:4]([H:7])([H:8])[CH:5]=[O:6]>>[CH3:1][CH:2]=[CH:
 
 
 # Parse Individual Chemical Graphs as NetworkX Objects
+def visualize_graph(rsmi:str):
+    educt_graph, product_graph = rsmi_to_graph(rsmi)
 
-educt_graph, product_graph = rsmi_to_graph(rsmi)
+    print("Eductgraph")
+    for n, d in educt_graph.nodes(data=True):
+        print(n, d)
+    for u,v, d in educt_graph.edges(data=True):
+        print(u,v, d)
 
-print("Eductgraph")
-for n, d in educt_graph.nodes(data=True):
-    print(n, d)
-for u,v, d in educt_graph.edges(data=True):
-    print(u,v, d)
+    print("Productgraph")
+    for n, d in product_graph.nodes(data=True):
+        print(n, d)
+    for u,v, d in product_graph.edges(data=True):
+        print(u,v, d)
 
-print("Productgraph")
-for n, d in product_graph.nodes(data=True):
-    print(n, d)
-for u,v, d in product_graph.edges(data=True):
-    print(u,v, d)
+    # Full ITS graph as NetworkX Object
+    full_graph = rsmi_to_its(rsmi, core=False)
 
-# Full ITS graph as NetworkX Object
-full_graph = rsmi_to_its(rsmi, core=False)
+    print("ITS graph")
+    for n, d in full_graph.nodes(data=True):
+        print(n, d)
+    for u,v, d in full_graph.edges(data=True):
+        print(u,v, d)
 
-print("ITS graph")
-for n, d in full_graph.nodes(data=True):
-    print(n, d)
-for u,v, d in full_graph.edges(data=True):
-    print(u,v, d)
-
-# Visualize ITS
-viz = GraphVisualizer()
-fig = viz.visualize_its(full_graph, use_edge_color=True)
-plt.show()
+    # Visualize ITS
+    viz = GraphVisualizer()
+    fig = viz.visualize_its(full_graph, use_edge_color=True)
+    plt.show()
 
